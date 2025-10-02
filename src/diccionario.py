d74fbef5bcb09ed43d55d81295b8ecdf911c478a
unknown <monica.ceron@correounivalle.edu.co>
2025-10-02 18:31:19 -0500

    diccionario

---
+++ src/diccionario.py
@@ -0,0 +1,13 @@
+''
+edades_amigos={"wilmar": 30,
+               "omar": 35,
+               "dario":20,
+               }
+print("edades_amigos:", edades_amigos)
+print("la edad de wilmar es:", 30 )
+
+
+
+
+
+
