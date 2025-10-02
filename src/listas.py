afc62feac35ab37eef68a314ee9422df074c5dd3
unknown <monica.ceron@correounivalle.edu.co>
2025-10-02 18:11:29 -0500

    listas

---
+++ src/listas.py
@@ -0,0 +1,32 @@
+''
+nombres= ["lian",
+          "maria",
+          "pedro"]
+        
+
+
+#print("estos son mis amigos:" , nombres)
+# sobre estas listas puedo iterar 
+
+amigos=["lian","maria","pedro"]
+#print("la longitud de la lista es:",len(amigos))
+
+#print("primer nombre:", nombres[0])
+#print("primer segundo nombre:", nombres[1])
+
+#print("amigos:", amigos[2:])
+
+nombres = ["Lian", "Ana", "Juan", "Pedro"]
+
+#print("Primer nombre en mayúscula:", nombres[0].upper())
+
+
+
+
+
+
+
+
+
+
+
