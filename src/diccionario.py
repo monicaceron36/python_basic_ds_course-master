30e475cdbe186edc2eb90ee6b9ff7289bd10ba2d
unknown <monica.ceron@correounivalle.edu.co>
2025-10-14 17:31:37 -0500

    clase tuplas y cojuntos

--- src/diccionario.py
+++ src/diccionario.py
@@ -3,8 +3,11 @@ edades_amigos={"wilmar": 30,
                "omar": 35,
                "dario":20,
                }
-print("edades_amigos:", edades_amigos)
-print("la edad de wilmar es:", 30 )
+#("edades_amigos:", edades_amigos)
+#print("la edad de wilmar es:", 30 )
+
+
+
 
 
 
---
+++ src/tuplas.py
@@ -0,0 +1,20 @@
+# que diferencia hay entre tupla Y lista pues bien la tupla son inmutable y la lista son mutables
+
+num10_1tupla= (1,2,3,4,5,6,7,8,9,10)  # aqui ya se convierte en tupla datos que yo no quiero modifiar para eso es la tuplas
+
+print(
+num10_1tupla)
+
+num10_1lista= [1,2,3,4,5,6,7,8,9,10]
+num10_1lista[0]=100
+
+print(num10_1lista)
+
+
+
+
+
+
+
+
+
