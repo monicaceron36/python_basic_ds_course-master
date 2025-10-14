9dc2cf8f5a8f501aa44baffc51eed4e9885f75f0
unknown <monica.ceron@correounivalle.edu.co>
2025-10-14 18:14:51 -0500

    conjuntos

---
+++ src/conjuntos.py
@@ -0,0 +1,21 @@
+'' # lo defino con llaves, por que no es un diccionario por que el diccionario es llaves y value 
+
+num1_10_set= {1,2,3,4,5,6,7,8,9,10}
+#print(type(num10_1tupla))
+
+# regla pep 8 de pytho donde la variable se debe definir con _ 
+
+#num10_pares_set={2,4,6,8,12,14}
+#print(num10_pares_set)
+
+#se pueden unir conjuntos y tambin hacer interseccion la diferencia es ara saber que nuemros faltan 
+
+#print(num1_10_set-num10_pares_set)
+
+
+
+
+
+
+
+
